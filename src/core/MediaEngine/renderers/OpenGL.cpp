55b5e6511b81c7ca9b0b02b7b6b4eaefef63e0cc
SamTheDev <vs.samu.dev@gmail.com>
2023-05-23 18:07:30 +0200

    just saving

--- src/core/MediaEngine/renderers/OpenGL.cpp
+++ src/core/MediaEngine/renderers/OpenGL.cpp
@@ -121,7 +121,7 @@ namespace GameEngine::MEDIA::RENDERER
         // Clear Screen
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glClearColor(r, g, b, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
     }
 
     std::string OpenGL::add(std::string name, shader s)
--- src/core/MediaEngine/windowers/glfw.cpp
+++ src/core/MediaEngine/windowers/glfw.cpp
@@ -121,8 +121,8 @@ namespace GameEngine::MEDIA::WINDOWER
         glfwMakeContextCurrent(window);
 
         // Init OpenGL
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
             
         // Load OpenGL
