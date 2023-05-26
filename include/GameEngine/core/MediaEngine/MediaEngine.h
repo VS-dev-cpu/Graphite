92572b0dc5a20cb2446e7db8e2cc3b01d2abc9f6
SamTheDev <vs.samu.dev@gmail.com>
2023-05-26 16:42:25 +0200

    started integrating Artifex

--- include/GameEngine/core/MediaEngine/MediaEngine.h
+++ include/GameEngine/core/MediaEngine/MediaEngine.h
@@ -13,91 +13,88 @@
 // Renderers
 #include <GameEngine/core/MediaEngine/renderers/OpenGL.h>
 
-#include <atomic>
-#include <vector>
-#include <variant>
-#include <tuple>
 #include <any>
+#include <atomic>
+#include <tuple>
+#include <variant>
+#include <vector>
 
-namespace GameEngine::MEDIA
-{
-    class GameEngine;
+namespace GameEngine::MEDIA {
+class GameEngine;
 
-    // RenderQueue Action
-    namespace ACTION
-    {
-        enum ENUM
-        {
-            // Basic Management
-            PASS,  // Do Nothing [TM] (void)
-            SLEEP, // Sleep Some Time (float)
-            PRINT, // Print out a String (std::string)
+// RenderQueue Action
+namespace ACTION {
+enum ENUM {
+  // Basic Management
+  PASS,  // Do Nothing [TM] (void)
+  SLEEP, // Sleep Some Time (float)
+  PRINT, // Print out a String (std::string)
 
-            // Resource Loading
-            ADD_SHADER,  // Add Shader (std::pair<std::string, shader>)
-            ADD_TEXTURE, // Add Texture std::pair<std::string, texture>
-            ADD_MESH,    // Add Mesh (TODO)
+  // Resource Loading
+  ADD_SHADER,  // Add Shader (std::pair<std::string, shader>)
+  ADD_TEXTURE, // Add Texture std::pair<std::string, texture>
+  ADD_MESH,    // Add Mesh (TODO)
 
-            // Resource Freeing
-            FREE_SHADER,  // Free Shader (std::string)
-            FREE_TEXTURE, // Free Texture (std::string)
-            FREE_MESH,    // Free Mesh (TODO)
+  // Resource Freeing
+  FREE_SHADER,  // Free Shader (std::string)
+  FREE_TEXTURE, // Free Texture (std::string)
+  FREE_MESH,    // Free Mesh (TODO)
 
-            // Rendering
-            CLEAR,        // Clear Screen (vec3)
-            DRAW_TEXTURE, // Render Image
-            DRAW_MESH,    // Render Mesh
+  // Rendering
+  CLEAR,        // Clear Screen (vec3)
+  DRAW_TEXTURE, // Render Image
+  DRAW_MESH,    // Render Mesh
 
-            // TODO: Add More Functions
-        };
-    }
-
-    typedef std::any RenderData;
-    typedef std::pair<ACTION::ENUM, RenderData> RenderTask;
-
-    //  The Media Engine (responsible for Windowing, Rendering and Audio)
-    class MediaEngine
-    {
-    public:
-        // Initialize Media Engine
-        MediaEngine(std::string name = "MediaEngine", bool fullscreen = true, int width = 720, int height = 480);
-
-        // Destroy Media Engine
-        ~MediaEngine();
-
-        // Sync With RenderThread & Update RenderQueue
-        void update();
-
-        // Exit Render Loop
-        void exit();
-
-        // Add Action to Queue
-        void add(ACTION::ENUM action, RenderData data);
-
-    public:
-        // Window Properties
-        std::string name;
-        bool fullscreen;
-        int width, height;
-
-        std::atomic<bool> running = false;
-        std::atomic<bool> quit = false;
-
-        Windower* windower;
-    private:
-        // Render Thread
-        pthread_t renderThread;
-
-        pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-        pthread_cond_t renderSignal = PTHREAD_COND_INITIALIZER;
-
-        std::atomic<bool> drawable = false;
-
-        Renderer* renderer;
-
-        std::vector<RenderTask> renderQueue;
-
-    private:
-        static void *render(void *arg);
-    };
+  // TODO: Add More Functions
+};
 }
+
+typedef std::any RenderData;
+typedef std::pair<ACTION::ENUM, RenderData> RenderTask;
+
+//  The Media Engine (responsible for Windowing, Rendering and Audio)
+class MediaEngine {
+public:
+  // Initialize Media Engine
+  MediaEngine(std::string name = "MediaEngine", bool fullscreen = true,
+              int width = 720, int height = 480);
+
+  // Destroy Media Engine
+  ~MediaEngine();
+
+  // Sync With RenderThread & Update RenderQueue
+  void update();
+
+  // Exit Render Loop
+  void exit();
+
+  // Add Action to Queue
+  void add(ACTION::ENUM action, RenderData data);
+
+public:
+  // Window Properties
+  std::string name;
+  bool fullscreen;
+  int width, height;
+
+  std::atomic<bool> running = false;
+  std::atomic<bool> quit = false;
+
+  // Windower* windower;
+private:
+  // Render Thread
+  pthread_t renderThread;
+
+  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_cond_t renderSignal = PTHREAD_COND_INITIALIZER;
+
+  std::atomic<bool> drawable = false;
+
+  // Renderer *renderer;
+
+  std::vector<RenderTask> renderQueue;
+
+private:
+  static void *render(void *arg);
+};
+} // namespace GameEngine::MEDIA
--- src/core/MediaEngine/MediaEngine.cpp
+++ src/core/MediaEngine/MediaEngine.cpp
@@ -1,164 +1,139 @@
+#include "Artifex/Artifex.h"
 #include <GameEngine/core/MediaEngine/MediaEngine.h>
 
-namespace GameEngine::MEDIA
-{
-    MediaEngine::MediaEngine(std::string name, bool fullscreen, int width, int height) : name(name), fullscreen(fullscreen), width(width), height(height)
-    {
-        // Initialize Windower & Renderer
-        windower = new WINDOWER::GLFW;
-        renderer = new RENDERER::OpenGL;
+namespace GameEngine::MEDIA {
+MediaEngine::MediaEngine(std::string name, bool fullscreen, int width,
+                         int height)
+    : name(name), fullscreen(fullscreen), width(width), height(height) {
+    // Initialize Windower & Renderer
+    // windower = new WINDOWER::GLFW;
+    // renderer = new RENDERER::OpenGL;
 
-        running = true;
+    running = true;
 
-        // Initialize Render Thread
-        if (pthread_create(&renderThread, nullptr, &render, this))
-            LOG::ERROR("MediaEngine", "Render Thread Failed");
-        else
-            LOG::SYSTEM("MediaEngine", "Renderer Initialized");
-    }
+    // Initialize Render Thread
+    if (pthread_create(&renderThread, nullptr, &render, this))
+        LOG::ERROR("MediaEngine", "Render Thread Failed");
+    else
+        LOG::SYSTEM("MediaEngine", "Renderer Initialized");
+}
 
-    MediaEngine::~MediaEngine()
-    {
-        // Stop Threads
-        running = false;
-        pthread_join(renderThread, NULL);
-    }
+MediaEngine::~MediaEngine() {
+    // Stop Threads
+    running = false;
+    pthread_join(renderThread, NULL);
+}
 
-    void MediaEngine::update()
-    {
-        drawable = true;
-        while (drawable && running && !quit)
+void MediaEngine::update() {
+    drawable = true;
+    while (drawable && running && !quit)
+        ;
+
+    renderQueue.clear();
+}
+
+void MediaEngine::exit() { quit = true; }
+
+void MediaEngine::add(ACTION::ENUM action, RenderData data) {
+    renderQueue.push_back(RenderTask(action, data));
+}
+
+void *MediaEngine::render(void *arg) {
+    // Detach Thread
+    MediaEngine *engine = (MediaEngine *)arg;
+    pthread_detach(pthread_self());
+
+    // Initialize Renderer
+    Artifex ax(engine->name, engine->width, engine->height);
+    ax.fullscreen(engine->fullscreen);
+
+    // Main Render Loop
+    while (engine->running) {
+        while (!engine->drawable && engine->running && !engine->quit)
             ;
 
-        renderQueue.clear();
-    }
+        auto tasks = engine->renderQueue; // TODO: fix RenderQueue Issue
+        engine->drawable = false;
 
-    void MediaEngine::exit()
-    {
-        quit = true;
-    }
+        // Draw Render Queue (content)
+        for (auto [action, data] : tasks) {
+            switch (action) {
+            default:
+                // Invalid ENUM
+                LOG::WARNING("RenderThread", "Invalid Enum: %i", action);
+            case ACTION::PASS: {
+                // Do Nothing
+            } break;
 
-    void MediaEngine::add(ACTION::ENUM action, RenderData data)
-    {
-        renderQueue.push_back(RenderTask(action, data));
-    }
+            case ACTION::SLEEP: {
+                // Sleep
+                float amount = std::any_cast<float>(data);
+                float start = time();
+                while (time() - start < amount)
+                    ;
+            } break;
 
-    void *MediaEngine::render(void *arg)
-    {
-        // Detach Thread
-        MediaEngine *engine = (MediaEngine *)arg;
-        pthread_detach(pthread_self());
+            case ACTION::PRINT: {
+                // Print Debug Message
+                std::string str = std::any_cast<std::string>(data);
+                LOG::DEBUG("RenderQueue", str.c_str());
+            } break;
 
-        // Initialize Windower
-        if (!engine->windower->init(engine->name, engine->fullscreen, engine->width, engine->height))
-            LOG::ERROR("MediaEngine", "Failed to init Windower");
+            case ACTION::CLEAR: {
+                // Clear Screen
+                vec3 color = std::any_cast<vec3>(data);
+                ax.clear(color.x, color.y, color.z);
+            } break;
 
-        // Initialize Renderer
-        if (!engine->renderer->init(engine->windower))
-            LOG::ERROR("MediaEngine", "Failed to init Renderer");
+            case ACTION::ADD_SHADER: {
+                // Add Shader
+                std::pair<std::string, shader> sh =
+                    std::any_cast<std::pair<std::string, shader>>(data);
+                ax.shader(sh.second.vertex, sh.second.fragment,
+                          sh.second.geometry);
+            } break;
 
-        // Main Render Loop
-        while (engine->running)
-        {
-            while (!engine->drawable && engine->running && !engine->quit)
-                ;
+            case ACTION::ADD_TEXTURE: {
+                // Add Texture
+                std::pair<std::string, texture> tex =
+                    std::any_cast<std::pair<std::string, texture>>(data);
+                engine->renderer->add(tex.first, tex.second);
+            } break;
 
-            auto tasks = engine->renderQueue; // TODO: fix RenderQueue Issue
-            engine->drawable = false;
+            case ACTION::DRAW_TEXTURE: {
+                // Draw Texture
+                auto [name, center, size, rotation] =
+                    std::any_cast<std::tuple<std::string, vec2, vec2, float>>(
+                        data);
+                engine->renderer->draw_texture(name, center, size, rotation);
+            } break;
 
-            // Draw Render Queue (content)
-            for (auto [action, data] : tasks)
-            {
-                switch (action)
-                {
-                default:
-                    // Invalid ENUM
-                    LOG::WARNING("RenderThread", "Invalid Enum: %i", action);
-                case ACTION::PASS:
-                {
-                    // Do Nothing
-                }
-                break;
-
-                case ACTION::SLEEP:
-                {
-                    // Sleep
-                    float amount = std::any_cast<float>(data);
-                    float start = time();
-                    while (time() - start < amount)
-                        ;
-                }
-                break;
-
-                case ACTION::PRINT:
-                {
-                    // Print Debug Message
-                    std::string str = std::any_cast<std::string>(data);
-                    LOG::DEBUG("RenderQueue", str.c_str());
-                }
-                break;
-
-                case ACTION::CLEAR:
-                {
-                    // Clear Screen
-                    vec3 color = std::any_cast<vec3>(data);
-                    engine->renderer->clear(color.x, color.y, color.z);
-                }
-                break;
-
-                case ACTION::ADD_SHADER:
-                {
-                    // Add Shader
-                    std::pair<std::string, shader> sh = std::any_cast<std::pair<std::string, shader>>(data);
-                    engine->renderer->add(sh.first, sh.second);
-                }
-                break;
-
-                case ACTION::ADD_TEXTURE:
-                {
-                    // Add Texture
-                    std::pair<std::string, texture> tex = std::any_cast<std::pair<std::string, texture>>(data);
-                    engine->renderer->add(tex.first, tex.second);
-                }
-                break;
-
-                case ACTION::DRAW_TEXTURE:
-                {
-                    // Draw Texture
-                    auto [name, center, size, rotation] = std::any_cast<std::tuple<std::string, vec2, vec2, float>>(data);
-                    engine->renderer->draw_texture(name, center, size, rotation);
-                }
-                break;
-
-                case ACTION::FREE_SHADER:
-                {
-                    // Free Shader
-                    std::string sh = std::any_cast<std::string>(data);
-                    engine->renderer->free_shader(sh);
-                }
-
-                case ACTION::FREE_TEXTURE:
-                {
-                    // Free Texture
-                    std::string tex = std::any_cast<std::string>(data);
-                    engine->renderer->free_texture(tex);
-                }
-                break;
-                }
+            case ACTION::FREE_SHADER: {
+                // Free Shader
+                std::string sh = std::any_cast<std::string>(data);
+                engine->renderer->free_shader(sh);
             }
 
-            // Update Screen
-            engine->running = engine->renderer->update();
-            tasks.clear();
-
-            if (engine->quit)
-                engine->running = false;
+            case ACTION::FREE_TEXTURE: {
+                // Free Texture
+                std::string tex = std::any_cast<std::string>(data);
+                engine->renderer->free_texture(tex);
+            } break;
+            }
         }
 
-        engine->renderer->clean();
+        // Update Screen
+        engine->running = engine->renderer->update();
+        tasks.clear();
 
-        // exit thread
-        pthread_exit(nullptr);
-        return nullptr;
+        if (engine->quit)
+            engine->running = false;
     }
+
+    engine->renderer->clean();
+
+    // exit thread
+    pthread_exit(nullptr);
+    return nullptr;
 }
+} // namespace GameEngine::MEDIA
