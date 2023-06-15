dc09be5b903b173e1ff893b2c657fb8139a1f5da
SamTheDev <vs.samu.dev@gmail.com>
2023-06-15 18:46:45 +0200

    grouped opengl-specific code together

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,9 +1,7 @@
 #include <Graphite/core/ResourceManager.h>
 
-#include <GL/glad.h>
-
 #define STB_IMAGE_IMPLEMENTATION
-#include <GL/stb_image.h>
+#include <Graphite/core/stb_image.h>
 
 #include <cstring>
 #include <fstream>
